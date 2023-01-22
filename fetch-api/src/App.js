4ad9c7b08a29cf15c5ea76672fe7770195d3aa08
Rijo George <rijogeorge@Rijos-MacBook-Pro.local>
2023-01-22 15:35:55 -0800

    fetching

--- fetch-api/src/App.js
+++ fetch-api/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
 
 function App() {
+  const [catFact, setCatFact] = React.useState("");
+  const fetchData = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCatFact(data.fact);
+      });
+  };
+  useEffect(fetchData, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>Generate cat fact</button>
+      <p> {catFact} </p>
     </div>
   );
 }
